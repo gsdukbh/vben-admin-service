35c598fe28454cadaa04da75092577cda4d26f51
JiaWei Lee <gsdukbh@gmail.com>
2022-02-18 17:33:45 +0800

    add

--- src/main/java/top/werls/vben/system/entity/SysRole.java
+++ src/main/java/top/werls/vben/system/entity/SysRole.java
@@ -1,5 +1,6 @@
 package top.werls.vben.system.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,8 +14,21 @@ public class SysRole extends BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 211L;
 
-    private Long roleId;
+    private Long rid;
+    /**
+     * 角色名称
+     */
+    @Schema(description = "角色名称")
     private String roleName;
+    /**
+     * 角色编码
+     */
+    @Schema(description = "角色编码")
     private String value;
+    /**
+     * 角色描述
+     */
+    @Schema(description = "角色描述")
+    private String description;
     
 }
--- src/main/java/top/werls/vben/system/vo/UserInfoVo.java
+++ src/main/java/top/werls/vben/system/vo/UserInfoVo.java
@@ -19,8 +19,51 @@ public class UserInfoVo implements java.io.Serializable {
     @Serial
     private static final long serialVersionUID = -72618660139164414L;
 
-    @Schema(description = "用户信息")
-    private SysUser user;
+    /**
+     * 用户ID
+     */
+    @Schema(description = "用户ID")
+    private Long userId;
+    /**
+     * 用户名
+     */
+    @Schema(description = "用户名")
+    private String username;
+    /**
+     * 用户昵称
+     */
+    @Schema(description = "用户昵称")
+    private String realName;
+    /**
+     * 用户头像
+     */
+    @Schema(description = "用户头像")
+    private String avatar;
+    /**
+     * 用户主页
+     */
+    @Schema(description = "用户主页")
+    private String homePath;
+    /**
+     * 角色列表
+     */
     @Schema(description = "角色信息")
     private List<SysRole> roles;
+
+    public UserInfoVo() {
+    }
+
+    public UserInfoVo(SysUser user) {
+        this.userId = user.getUid();
+        this.username = user.getUsername();
+        this.realName = user.getRealName();
+        this.avatar = user.getAvatar();
+    }
+    public UserInfoVo(SysUser user, List<SysRole> roles) {
+        this.userId = user.getUid();
+        this.username = user.getUsername();
+        this.realName = user.getRealName();
+        this.avatar = user.getAvatar();
+        this.roles = roles;
+    }
 }
