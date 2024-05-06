73cf72ade6e626b3228fcb27add6c827d7c5827b
JiaWei Lee <gsdukbh@gmail.com>
2024-05-06 11:57:19 +0800

    refactor: 移除 MybatisPlus ,更新依赖版本。

--- src/main/java/top/werls/vben/config/SecurityConfig.java
+++ src/main/java/top/werls/vben/config/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,59 +20,54 @@ import top.werls.vben.system.Security.CustomizeAccessDeniedHandler;
 import top.werls.vben.system.Security.CustomizeAuthEntryPoint;
 import top.werls.vben.system.Security.JwtAuthenticationTokenFilter;
 
-
-
 @Configuration
-public class SecurityConfig  {
+public class SecurityConfig {
 
+  @Resource private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
-    @Resource
-    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+  @Resource private CustomizeAuthEntryPoint authEntryPoint;
+  @Resource private CustomizeAccessDeniedHandler accessDeniedHandler;
 
-    @Resource
-    private CustomizeAuthEntryPoint authEntryPoint;
-    @Resource
-    private CustomizeAccessDeniedHandler accessDeniedHandler;
+  @Value("${env.isEnableSwagger}")
+  private boolean isEnableSwagger;
 
-    @Value("${env.isEnableSwagger}")
-    private boolean isEnableSwagger;
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+  @Bean
+  public AuthenticationManager CustomAuthenticationManager(AuthenticationConfiguration auth)
+      throws Exception {
+    return auth.getAuthenticationManager();
+  }
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    if (isEnableSwagger) {
+      http.authorizeHttpRequests(
+          requests ->
+              requests
+                  .requestMatchers("/swagger-ui.html", "/webjars/**", "/swagger-ui*/**", "/v3/**")
+                  .permitAll());
     }
-
-    @Bean
-    public AuthenticationManager CustomAuthenticationManager(AuthenticationConfiguration auth)
-            throws Exception {
-        return auth.getAuthenticationManager();
-    }
-
-
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        if (isEnableSwagger) {
-            http.authorizeHttpRequests().requestMatchers("/swagger-ui.html",
-                    "/webjars/**",
-                    "/swagger-ui*/**",
-                    "/v3/**").permitAll();
-        }
-        http.
-                cors()
-                .and()
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-                .and().logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
-                .and()
-                .exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler)
-                .authenticationEntryPoint(authEntryPoint)
-                .and()
-                .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-        ;
-        return  http.build();
-    }
+    http.cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            requests -> requests.requestMatchers("/login").permitAll().anyRequest().authenticated())
+        .exceptionHandling(
+            authorizeRequests ->
+                authorizeRequests
+                    .accessDeniedHandler(accessDeniedHandler)
+                    .authenticationEntryPoint(authEntryPoint))
+        .logout(
+            logout ->
+                logout.logoutSuccessHandler(
+                    new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK)))
+        .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
+        .sessionManagement(
+            sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+    return http.build();
+  }
 }
